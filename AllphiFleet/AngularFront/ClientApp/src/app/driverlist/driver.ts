48d76f41bb9553c07569520082cf3cd1d3fe0dab
Stefan Eeckhoudt <stefan.eeckhoudt@allphi.eu>
2020-12-04 12:26:44 +0100

    get drivers angular. service + layout

---
+++ AllphiFleet/AngularFront/ClientApp/src/app/driverlist/driver.ts
@@ -0,0 +1,23 @@
+interface IDriver {
+
+  id: number;
+  naam: string;
+  voorNaam: number;
+  geboorteDatum: Date;
+  rijksregisterNummer: number;
+  typeRijbewijs: number;
+  Actief: boolean;
+  AdresId: number;
+  TankkaartId: number;
+  /*
+  id: number;
+  name: string;
+  firstName: number;
+  dateOfBirth: Date;
+  SocialSecurityNumber: number;
+  DriverLicenceType: number;
+  Active: boolean;
+  AddressId: number;
+  GasCardId: number;
+  */
+}
--- AllphiFleet/AngularFront/ClientApp/src/app/driverlist/driverlist.component.ts
+++ AllphiFleet/AngularFront/ClientApp/src/app/driverlist/driverlist.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+
+import { DriverService } from './driver.service';
 
 @Component({
   selector: 'app-driverlist',
@@ -7,49 +8,38 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./driverlist.component.css']
 })
 export class DriverlistComponent implements OnInit {
-  public drivers: Driver[];
-  public tempUrl: string = "https://localhost:44334/api/chauffeur";
+  public drivers: IDriver[];
+  //public tempUrl: string = "https://localhost:44334/api/chauffeur";
+  errorMessage = '';
 
-  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
-    http.get<Driver[]>(this.tempUrl).subscribe(result => {
-      this.drivers = result;
-      //console.log(this.drivers);
-    }, error => console.error(error));
+  constructor(private driverService: DriverService) {
+
+  }
     /*
+  constructor(http: HttpClient, @Inject('BASE_URL') baseUrl: string) {
+  
+      http.get<Driver[]>(this.tempUrl).subscribe(result => {
+      this.drivers = result;
+      console.log(this.drivers);
+    }, error => console.error(error));
+
     http.get<Driver[]>(baseUrl + 'drivers').subscribe(result => {
       this.drivers = result;
     }, error => console.error(error));
-    */
+    
 
     console.log(baseUrl + 'drivers');
-  }
+  }*/
+
   ngOnInit(): void {
 
-    throw new Error("Method not implemented.");
+    this.driverService.getDrivers().subscribe({
+      next: drivers => {
+        this.drivers = drivers;
+        //this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
   }
 
-}
-
-interface Driver {
-
-  id: number;
-  naam: string;
-  voorNaam: number;
-  geboorteDatum: Date;
-  rijksregisterNummer: number;
-  typeRijbewijs: number;
-  Actief: boolean;
-  AdresId: number;
-  TankkaartId: number;
-  /*
-  id: number;
-  name: string;
-  firstName: number;
-  dateOfBirth: Date;
-  SocialSecurityNumber: number;
-  DriverLicenceType: number;
-  Active: boolean;
-  AddressId: number;
-  GasCardId: number;
-  */
 }
